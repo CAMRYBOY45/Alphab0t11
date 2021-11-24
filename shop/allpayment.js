a27fa5415be1199a4e641f687a63c393c8a69649
Mr.C1TY3920 <44989007+CAMRYBOY45@users.noreply.github.com>
2021-11-24 12:40:23 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,31 +1,24 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY GaciL STORE
 
 *🏧 BANK*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+Tidak tersedia
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ -
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0878 7018 0306 8
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0878 7018 0306 8
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama G***l
 
 *📲 PULSA*
 
-Tidak tersedia
+ 62878701803068
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
